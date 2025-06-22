19d5032be740d4b9b62aed22e1bb86af7ffa64de
Juan Coelho <juancoelho25@hotmail.com>
2025-06-22 13:14:11 -0300

    testes E2E adicionados

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -68,4 +68,69 @@ describe('TODOMvc App', () => {
       .children()
       .should('have.length', 2);
   });
+  });
+
+describe('TODOMvc App - Testes Complementares', () => {
+  beforeEach(() => {
+    cy.visit('');
+  });
+
+  it('Edita uma tarefa existente', () => {
+    cy.get('[data-cy=todo-input]').type('Tarefa original{enter}');
+
+    cy.get('[data-cy=todos-list] li')
+      .first()
+      .dblclick();
+
+    cy.get('li.editing .edit')
+      .clear()
+      .type('Tarefa editada com sucesso{enter}');
+
+    cy.get('[data-cy=todos-list] li')
+      .should('have.length', 1)
+      .and('contain.text', 'Tarefa editada com sucesso');
+
+    cy.get('li.editing').should('not.exist');
+  });
+
+  it('Marca e desmarca uma tarefa como concluída', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('Revisar código{enter}');
+
+    cy.get('[data-cy=toggle-todo-checkbox]')
+      .check()
+      .should('be.checked');
+
+    cy.get('[data-cy=toggle-todo-checkbox]')
+      .uncheck()
+      .should('not.be.checked');
+  });
+
+  it('Limpa as tarefas que foram completadas', () => {
+    cy.get('[data-cy=todo-input]').type('Tarefa 1 - Fazer{enter}');
+    cy.get('[data-cy=todo-input]').type('Tarefa 2 - Remover{enter}');
+    cy.get('[data-cy=todo-input]').type('Tarefa 3 - Remover{enter}');
+
+    cy.get('[data-cy=todos-list] li')
+      .eq(1)
+      .find('.toggle')
+      .click();
+
+    cy.get('[data-cy=todos-list] li')
+      .eq(2)
+      .find('.toggle')
+      .click();
+
+    cy.get('.clear-completed').should('be.visible').click();
+
+    cy.get('[data-cy=todos-list] li').should('have.length', 1);
+
+    cy.get('[data-cy=todos-list] li')
+      .first()
+      .should('have.text', 'Tarefa 1 - Fazer');
+
+    cy.get('.clear-completed').should('not.be.visible');
+  });
 });
